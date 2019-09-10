23d6d90558296b2265139642831b8a86cbb1b3a4
Eloi <hoquei0021@gmail.com>
2019-09-10 18:03:40 +0200

    LCD L2C Corrections

--- autocons.ino
+++ autocons.ino
@@ -16,7 +16,7 @@ EnergyMonitor emon3;             // 4 Idem que dessus pour une troisième pince 
 #include <Wire.h>                        // 2 inclure la librairie "wire.h" (si elle n'existe pas dans la bibliothèque, faire comme expliqué pour EmonLib.h
 #include <LiquidCrystal_I2C.h>           // 2 inclure la librairie "LiquidCrystal_I2C.h" 
 //LiquidCrystal_I2C lcd(0x27, 16, 4);    // 2 définir l'adresse de l'écran LCD, le nombre de caractères et le nombre de lignes
-LiquidCrystal_I2C lcd(0x3f,16,2);        // 2 Si Le LCD ne fonctionne pas, alors mettre // sur la ligne précédente et enlever les // sur cette ligne
+LiquidCrystal_I2C lcd(0x27,16,2);        // 2 Si Le LCD ne fonctionne pas, alors mettre // sur la ligne précédente et enlever les // sur cette ligne
 
 //#include <DallasTemperature.h>           // 7 librairie de la sonde Dallas
 //#include <OneWire.h>                     // 7 librairie qui va gérer les différente adresses des sondes de température
@@ -62,17 +62,17 @@ void setup()
 {
   Serial.begin(9600);                 // 1 Création du port série pour que l'arduino puisse envoyer des infos vers l'ordinateur
 
-  emon1.voltage(0, 357, 1.7);         // 1 Initialisation du Voltage (Pin A2, Valeur à changer pour etalonnage (+/-357 pour 6v et +/- 190 pour 12v))
-  emon1.current(1, 5.2);             // 1 Initialisation du Courant en ampère ( Pin A1, Valeur à changer lors de l'etalonnage)
+  emon1.voltage(0, 107, 1.7);         // 1 Initialisation du Voltage (Pin A2, Valeur à changer pour etalonnage (+/-357 pour 6v et +/- 190 pour 12v))
+  emon1.current(3, 50);             // 1 Initialisation du Courant en ampère ( Pin A1, Valeur à changer lors de l'etalonnage)
   
-  emon2.voltage(0, 357, 1.7);         // 4 Initialisation du Voltage (Pin A2, Valeur à changer pour etalonnage, phase_shift)
-  emon2.current(2, 5.2);             // 4 Initialisation du Courant en ampère ( Pin A3, Valeur à changer lors de l'etalonnage)
+  emon2.voltage(0, 107, 1.7);         // 4 Initialisation du Voltage (Pin A2, Valeur à changer pour etalonnage, phase_shift)
+  emon2.current(1, 7.5);             // 4 Initialisation du Courant en ampère ( Pin A3, Valeur à changer lors de l'etalonnage)
   
-  emon3.voltage(0, 357, 1.7);         // 4 Initialisation du Voltage (Pin A2, Valeur à changer pour etalonnage, phase_shift)
-  emon3.current(3, 32);             // 4 Initialisation du Courant en ampère ( Pin A6, Valeur à changer lors de l'etalonnage)
+  emon3.voltage(0, 107, 1.7);         // 4 Initialisation du Voltage (Pin A2, Valeur à changer pour etalonnage, phase_shift)
+  emon3.current(2, 7.5);             // 4 Initialisation du Courant en ampère ( Pin A6, Valeur à changer lors de l'etalonnage)
 
   lcd.init();                         // 2 initialisation de l'afficheur LCD 
-
+lcd.backlight();                // 2 Allumer l'écran LCD
  //myservo.attach(5);                 // 3 On attache l'objet servo à la Pin D5 
  pinMode(led, OUTPUT);                // 6 La broche sur lequel est la variable led est une sortie 
 
@@ -107,17 +107,21 @@ void loop()
 
  //--------------------------Etalonnage des volts et ampères sans LCD--------------------------------------
 
-  Serial.print("Est-ce le bon voltage? ");      // 1 envoyer vers l'ordinateur le texte " Est-ce le bon voltage? "
+  /*Serial.print("Est-ce le bon voltage? ");      // 1 envoyer vers l'ordinateur le texte " Est-ce le bon voltage? "
   Serial.print(verif_voltage);                  // 1 envoyer vers l'ordinateur la valeur "verif_voltage (Vrms)"
-  Serial.print(" V:  ");                         // 1 envoyer vers l'ordinateur le caractère "V"
+  Serial.print(" -A1:  ");                         // 1 envoyer vers l'ordinateur le caractère "V"
   Serial.print(verif_ampere1);                   
-  Serial.print(" A1: ");
+  Serial.print(" -A2: ");
   Serial.print(verif_ampere2);                    
-  Serial.print(" A2: ");
-  Serial.print(verif_ampere3);                                       
-  Serial.print(" A3: ");
-  //Serial.print(Cos_phi);                   // 1 envoyer vers l'ordinateur la valeur "verif_voltage (Vrms)"                    
-  //Serial.print(" cos ");                          // 1 envoyer vers l'ordinateur le caractère "A"
+  Serial.print(" -A3: ");
+  Serial.print(verif_ampere3);                  
+  Serial.print("   ");
+  Serial.print(Cos_phi);                   
+  Serial.print("cos ");
+  Serial.print("Puissence reele 1: ");      // 1 envoyer vers l'ordinateur le texte " Est-ce le bon voltage? "
+  Serial.print(puissance_reelle1);
+  Serial.print(" Raw lecture: ");      // 1 envoyer vers l'ordinateur le texte " Est-ce le bon voltage? "*/
+  // Serial.println(analogRead(3));  
      
  //----------------POUR AVOIR LES W, Wh et kWh de l'élélectricité qui rentre et de l'électricité qui sort de ma maison------------------
   
@@ -195,17 +199,16 @@ void loop()
  analogWrite(led, intensite_led);  // 6 allumé ou éteint, c'est le dernier qui parle qui pilote l'intensité de la led
   
   
-  Serial.print(intensite_led);                //permet d'afficher sur l'ordinateur certaine valeur pertinantes pour la comprehention du programme
+  /*Serial.print(intensite_led);                //permet d'afficher sur l'ordinateur certaine valeur pertinantes pour la comprehention du programme
   Serial.print("   ");                        // cela permet aussi de voir ou en est le programme et de gérer les difficultés
   Serial.print(w_Chauffe_Eau);
-  Serial.print("   w");
+  Serial.print(" w");
+  Serial.println(" ");*/
   //Serial.print(TEMPERATURE0);
-  Serial.println(" C");
+  //Serial.println(" C");
   
   // --------------------ECRIRE SUR LCD ET POUR CHANGER LES VALEUR DuLCD AVEC UN BOUTON POUSSOIR------------------------------------------------
 
-//  lcd.backlight();                // 2 Allumer l'écran LCD
-  lcd.clear();                    // 2 rafraichir l'écran LCD ( efface les données précédentes)
   
  impulsion = digitalRead(pin_bouton_changer_ecran);                //5 Demande à "impulsion" de prendre la valeur de "pin_bouton_changer_ecran"
   if (impulsion == 0) compteur_impulsion = compteur_impulsion + 1; //5 On incrémente le compteur d'impulsion s'il y a une impulsion sur le bouton
